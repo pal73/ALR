5000bbea6f0d2fbb2d020707a4c6c7bbfd930c0d
pal <pal@ngs.ru>
2015-03-09 18:14:24 +0600

    second commit

--- pr3/src/com/company/Main.java
+++ pr3/src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("1234");
+        System.out.println("12345");
 	// write your code here
     }
 }
