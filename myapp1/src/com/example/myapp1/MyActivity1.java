cfb342ae24ba02740254ae39a18bece4d8be14b3
pal <pal@ngs.ru>
2015-03-09 17:44:42 +0600

    initial project version

---
+++ myapp1/src/com/example/myapp1/MyActivity1.java
@@ -0,0 +1,15 @@
+package com.example.myapp1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity1 extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
